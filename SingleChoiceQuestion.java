614495bb0f60f2beb2dbab347dfaab2e09322a2d
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-06-07 21:38:51 -0700

    Update SingleChoiceQuestion.java

--- SingleChoiceQuestion.java
+++ SingleChoiceQuestion.java
@@ -4,27 +4,26 @@ public class SingleChoiceQuestion implements Question {
     private String question;
     private String answer;
 
-    @Override
     public String setQuestionType() {
         return question;
     }
 
-    @Override
+
     public String setAnswerType() {
         return answer;
     } 
 
-    @Override
+
     public void setQuestion(String question) {
         this.question = question;
     }
 
-    @Override
+
     public void setAnswer(String answer) {
         this.answer = answer;
     } 
 
-    @Override
+
     public boolean multipleChoice() {
         return false;
     } 
