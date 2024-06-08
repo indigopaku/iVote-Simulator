7abb4ed11056cf37dc447878667e6d3f4308cd3c
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-06-07 22:42:49 -0700

    Update VotingService.java

--- VotingService.java
+++ VotingService.java
@@ -1,72 +1,48 @@
-import java.text.NumberFormat;
+package iVoteSimulator;
 
-// Get scores and display the results
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 public class VotingService {
 
     private Student[] students;
     private Question question;
-    private int A = 0, B = 0, C = 0, D = 0, r = 0, w = 0;
+    private Map<String, Integer> answerCounts;
 
-    // VotingService constructor with student & question objects
     public VotingService(Student[] students, Question question) {
         this.students = students;
         this.question = question;
-        counter(); // count answers
-        displayStats(); // display results
+        this.answerCounts = new HashMap<>();
+        countAnswers();
+        displayStats();
     }
 
-    public void counter() {
-        if (question.multipleChoice()) {
-            for (Student student : students) {
-                String answer = student.getAnswer();
-                if (answer.equals("A"))
-                    A++;
-                else if (answer.equals("B"))
-                    B++;
-                else if (answer.equals("C"))
-                    C++;
-                else if (answer.equals("D"))
-                    D++;
-            }
-        } else {
-            for (Student student : students) {
-                String answer = student.getAnswer();
-                if (answer.equals("True"))
-                    r++;
-                else if (answer.equals("False"))
-                    w++;
-            }
+    private void countAnswers() {
+        for (Student student : students) {
+            String answer = student.getAnswer();
+            answerCounts.put(answer, answerCounts.getOrDefault(answer, 0) + 1);
         }
     }
 
     public void displayStats() {
         System.out.println("Voting Service");
         System.out.println("-----------------");
-        System.out.println(question.setQuestionType());
+        System.out.println(question.getQuestionType());
         System.out.println(students.length + " students voted\n");
 
         System.out.println("Results");
         System.out.println("-----------------");
 
-        double percentage;
         NumberFormat percentFormat = NumberFormat.getPercentInstance();
+        int totalVotes = students.length;
 
-        if (question.multipleChoice()) {
-            percentage = ((double) A / students.length);
-            System.out.println("A: " + A + "    " + percentFormat.format(percentage));
-            percentage = ((double) B / students.length);
-            System.out.println("B: " + B + "    " + percentFormat.format(percentage));
-            percentage = ((double) C / students.length);
-            System.out.println("C: " + C + "    " + percentFormat.format(percentage));
-            percentage = ((double) D / students.length);
-            System.out.println("D: " + D + "    " + percentFormat.format(percentage) + "\n");
-        } else {
-            percentage = ((double) r / students.length);
-            System.out.println("1. True: " + r + "    " + percentFormat.format(percentage));
-            percentage = ((double) w / students.length);
-            System.out.println("2. False: " + w + "    " + percentFormat.format(percentage) + "\n");
+        for (String option : question.getAnswerType().split(",")) {
+            int count = answerCounts.getOrDefault(option, 0);
+            double percentage = (double) count / totalVotes;
+            System.out.println(option + ": " + count + "    " + percentFormat.format(percentage));
         }
 
-        System.out.println("Correct answer is " + question.setAnswerType());
+        System.out.println("Correct answer is " + question.getAnswerType());
     }
 }
