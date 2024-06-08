a152a5458a169a5fb8ea2025411c36cf95b0fc3a
indigopaku <108289555+indigopaku@users.noreply.github.com>
2024-06-07 21:45:23 -0700

    Update VotingService.java

--- VotingService.java
+++ VotingService.java
@@ -1,43 +1,45 @@
 import java.text.NumberFormat;
 
-//Get scores and display the results
+// Get scores and display the results
 public class VotingService {
 
     private Student[] students;
     private Question question;
     private int A = 0, B = 0, C = 0, D = 0, r = 0, w = 0;
 
-    //VotingService constructor with student & question objects
-    public VotingService(Student[] students, Question question){
+    // VotingService constructor with student & question objects
+    public VotingService(Student[] students, Question question) {
         this.students = students;
         this.question = question;
-        counter(); //count answers
-        displayStats(); //display results
+        counter(); // count answers
+        displayStats(); // display results
     }
-    
-    public void counter(){
-        if (question.multipleChoice()){
-            for (int i = 0; i < students.length; i++){
-                if(students[i].getAnswer() == "A")
+
+    public void counter() {
+        if (question.multipleChoice()) {
+            for (Student student : students) {
+                String answer = student.getAnswer();
+                if (answer.equals("A"))
                     A++;
-                else if(students[i].getAnswer() == "B")
+                else if (answer.equals("B"))
                     B++;
-                else if(students[i].getAnswer() == "C")
+                else if (answer.equals("C"))
                     C++;
-                else
-                    D++; 
+                else if (answer.equals("D"))
+                    D++;
             }
-        }
-        else
-        for(int i = 0; i < students.length; i++){
-            if (students[i].getAnswer().compareTo("True") == 0)
-            r++;
-            else
-            w++;
+        } else {
+            for (Student student : students) {
+                String answer = student.getAnswer();
+                if (answer.equals("True"))
+                    r++;
+                else if (answer.equals("False"))
+                    w++;
+            }
         }
     }
 
-    public void displayStats(){
+    public void displayStats() {
         System.out.println("Voting Service");
         System.out.println("-----------------");
         System.out.println(question.setQuestionType());
@@ -47,24 +49,24 @@ public class VotingService {
         System.out.println("-----------------");
 
         double percentage;
-        NumberFormat percentFormat=NumberFormat.getPercentInstance();
+        NumberFormat percentFormat = NumberFormat.getPercentInstance();
 
-         if (question.multipleChoice()){
-            percentage = ((double) A/students.length);
+        if (question.multipleChoice()) {
+            percentage = ((double) A / students.length);
             System.out.println("A: " + A + "    " + percentFormat.format(percentage));
-            percentage = ((double) B/students.length);
+            percentage = ((double) B / students.length);
             System.out.println("B: " + B + "    " + percentFormat.format(percentage));
-            percentage = ((double) C/students.length);
+            percentage = ((double) C / students.length);
             System.out.println("C: " + C + "    " + percentFormat.format(percentage));
-            percentage = ((double) D/students.length);
+            percentage = ((double) D / students.length);
             System.out.println("D: " + D + "    " + percentFormat.format(percentage) + "\n");
-        }
-         else{
-            percentage = ((double) r/students.length);
+        } else {
+            percentage = ((double) r / students.length);
             System.out.println("1. True: " + r + "    " + percentFormat.format(percentage));
-            percentage = ((double) w/students.length);
+            percentage = ((double) w / students.length);
             System.out.println("2. False: " + w + "    " + percentFormat.format(percentage) + "\n");
         }
+
         System.out.println("Correct answer is " + question.setAnswerType());
     }
 }
